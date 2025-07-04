c6c48ed67e4bd8cccd8ed013a8ac33cc0dd6a831
David Ferreira <davidleandro670@gmail.com>
2025-07-04 09:29:16 -0300

    ajustes

--- peso.js
+++ peso.js
@@ -7,19 +7,19 @@ IMC = weight / (height * height)
 
 
 if (IMC <= 15.99) {
-    console.log(`o seu IMC ( ${IMC} ) está categorizado como abaixo do peso de grau I`)
+    console.log(`o seu IMC ( ${IMC} ) está categorizado como abaixo do peso grau I`)
 } else if (IMC > 15.99 && IMC <= 16.99) {
-    console.log(`o seu IMC ( ${IMC} ) está categorizado como abaixo do peso de grau II`)
+    console.log(`o seu IMC ( ${IMC} ) está categorizado como abaixo do peso grau II`)
 } else if (IMC > 16.99 && IMC <= 18.49) {
-    console.log(`o seu IMC ( ${IMC} ) está categorizado como abaixo do peso de grau III`)
+    console.log(`o seu IMC ( ${IMC} ) está categorizado como abaixo do peso grau III`)
 } else if (IMC > 18.49 && IMC <= 24.99) {
     console.log(`o seu IMC ( ${IMC} ) está categorizado como peso adequado pelas medidas informadas`)
 } else if (IMC > 24.99 && IMC <= 29.99) {
     console.log(`o seu IMC ( ${IMC} ) está categorizado como sobrepeso`)
 } else if (IMC > 29.99 && IMC <= 34.99) {
-    console.log(`o seu IMC ( ${IMC} ) está categorizado como Obesidade de grau I`)
+    console.log(`o seu IMC ( ${IMC} ) está categorizado como Obesidade grau I`)
 } else if (IMC > 34.99 && IMC <= 39.99) {
-    console.log(`o seu IMC  ( ${IMC} ) está categorizado como Obesidade de grau II`)
+    console.log(`o seu IMC  ( ${IMC} ) está categorizado como Obesidade grau II`)
 } else if (IMC > 39.99) {
-    console.log(`o seu IMC ( ${IMC} ) está categorizado como Obesidade de grau III`)
+    console.log(`o seu IMC ( ${IMC} ) está categorizado como Obesidade grau III`)
 }
